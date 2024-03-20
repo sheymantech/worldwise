af144921367acbf7bc262accc00ac680eb1ef549
sheymantech <olaniyansheyi1704@gmail.com>
2024-03-20 23:59:49 +0100

    new feature

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
 import CountryList from "./components/countryList";
+import City from "./components/City";
 
 const BASE_URL = `http://localhost:9000`;
 
@@ -48,6 +49,9 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+
+          <Route path="cities/:id" element={<City />} />
+
           <Route
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
